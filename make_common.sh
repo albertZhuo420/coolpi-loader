3df3f39a01e5b8dc4bf42af9d1b1928dfd077a1a
albertZhuo420 <mr.zhuo420@outlook.com>
2025-06-12 13:54:49 +0800

    优化: 方便调试

--- make_common.sh
+++ make_common.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-if [[ -z "$PROJECT_ROOT" ]]; then
-	export PROJECT_ROOT_MAKE_SH=$(realpath "$(dirname "$0")/..")
+# ${CP_UBOOT_DBG+x} 会在 CP_UBOOT_DBG 已定义时展开成 x, 未定义时展开成空串
+# 用 -z 测试“展开后是空串”即可判断未定义
+if [[ ! -z "${CP_UBOOT_DBG+x}" ]]; then
+	if [[ -z "$PROJECT_ROOT" ]]; then
+		export PROJECT_ROOT_MAKE_SH=$(realpath "$(dirname "$0")/..")
+	fi
+	export PS4='+ $(realpath --relative-to="$PROJECT_ROOT_MAKE_SH" "${BASH_SOURCE[0]}"):${LINENO}: '
+	set -x
 fi
-export PS4='+ $(realpath --relative-to="$PROJECT_ROOT_MAKE_SH" "${BASH_SOURCE[0]}"):${LINENO}: '
-set -x
+
